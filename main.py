8a24f573ea556085155194dd11d0703123355e81
Mathew Sabu <125936934+TheLegend274@users.noreply.github.com>
2023-09-13 16:24:57 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import string 
+def caesar_ecrypt(message,key):
+    shift = key % 26
+    cipher = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[shift:]+ string.ascii_lowercase[:shift])
+    encrypted_message = message.lower().translate(cipher)
+
+    return encrypted_message
+
+
+def caesar_decrypt(encrypted_message, key):
+    shift = 26 - (key%26)
+    cipher = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[shift:]+ string.ascii_lowercase[:shift])
+
+    message = encrypted_message.translate(cipher)
+    return message
+
+message = 'Hello my name is Mathe Sabu'
+key = 3
+
+encrypted_message = caesar_ecrypt(message,key)
+print(f'Encrypted message: {encrypted_message}')
+
+
+
+decrypted_message = caesar_decrypt(encrypted_message,key)
+print(f'Decrypted message: {decrypted_message }')
+
+ 
+
+
